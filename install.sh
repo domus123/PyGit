3cafea77f43545d6f76fe6ba38ab3b4ce3d735e1
Lucas Guerra <lu.guerra7508@hotmail.com>
2016-10-17 09:44:34 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,20 @@ python create_db.py
 echo "Creating .commit ....."
 touch .commit
 
-echo "Coping .commit ...."
-sudo cp .commit /usr/local/bin
+#echo "Coping .commit ...."
+#sudo cp .commit /usr/local/bin
 
 echo "Cheating .fname ....."
 touch .fname
 
-echo "Coping .fname ....."
-sudo cp .fname /usr/local/bin
+#echo "Coping .fname ....."
+#sudo cp .fname /usr/local/bin
 
-mv pg.py pg
+#mv pg.py pg
 sudo cp pg /usr/local/bin 
 
 sudo cp functions.py /usr/local/bin
-sudo cp pg.db /usr/local/bin
+#sudo cp pg.db /usr/local/bin
 
 
 echo ""
